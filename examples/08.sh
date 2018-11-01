32b9debd80644ff4761ace2c6cca4a47274ab2ea
antoine.leveugle <antoine.leveugle@gmail.com>
2018-11-01 11:08:21 +0100

    feat: add reverse file example

---
+++ examples/08.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1/ Pattern space vs Hold space
+# h   # Copy pattern space to hold space
+# H   # Append pattern space to hold space
+# g   # Copy hold space to pattern space
+# G   # Append hold space to pattern space
+# x   # Exchange the contents of the hold and pattern spaces
+
+# eog ../reverse_file/01.png
+
+echo "1
+2
+3
+4" | \
+\
+sed ''
+

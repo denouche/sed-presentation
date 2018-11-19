e476d2a397d41e3e63bba96a7c27898868c546ad
antoine.leveugle <antoine.leveugle@gmail.com>
2018-11-19 16:53:11 +0100

    fix: fix bio missing loop in case of multiple lines

--- examples/00.sh
+++ examples/00.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 man sed | sed -r -n '/NAME/{
-    p
-    n
-    s/^(\s+)(sed) - /\1\2\n\n\1/
-    p
-    r bio.txt
+    p;:loop;n;s/^(\s+)(sed) - /\1\2\n\n\1/;p;/^$/!b loop;r ./bio.txt
 }'
 
